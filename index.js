4613e0fefcabd79133ae55533885846a68faf88f
Jay Landro <jaylandro@gmail.com>
2020-07-10 16:35:26 -0500

    More testing

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-try {
-  const labelName = context.payload.label;
-  const contentId = context.payload.issue.id;
-
-  if (labelName === "help wanted") {
-    github.projects.createCard({
-      column_id: 9962014,
-      content_id: contentId,
-      content_type: "Issue",
-    });
-  }
-
-  let contextHere = JSON.stringify(context.payload, null, 2);
-
-  console.log(contextHere);
-
-  return contextHere;
-} catch (error) {
-  new Error(error.message);
-}
-
 module.exports = ({ context }) => {
-  return context.payload.client_payload.value;
+  try {
+    const labelName = context.payload.label;
+    const contentId = context.payload.issue.id;
+    const contextHere = JSON.stringify(context.payload, null, 2);
+
+    if (labelName === "help wanted") {
+      github.projects.createCard({
+        column_id: 9962014,
+        content_id: contentId,
+        content_type: "Issue",
+      });
+    }
+
+    console.log(contextHere);
+
+    return contextHere;
+  } catch (error) {
+    new Error(error.message);
+  }
 };
