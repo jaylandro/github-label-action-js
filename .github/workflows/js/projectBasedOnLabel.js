ce9b56b072942f20698726470b0f51b12dc2de5d
Jay Landro <jaylandro@gmail.com>
2020-07-14 08:15:46 -0500

    Map label names

--- .github/workflows/js/projectBasedOnLabel.js
+++ .github/workflows/js/projectBasedOnLabel.js
@@ -1,3 +1,22 @@
 module.exports = ({ context }) => {
-  return context.payload;
+  const issue = context.payload.issue;
+  const labelsAsStrings = issue.labels.map((label) => label.name);
+
+  const workflowIssue = ["c: SQL Editor"].some((label) =>
+    labelsAsStrings.includes(label)
+  );
+
+  const priorityExists = ["P0", "P1", "P2", "P3"].some((label) =>
+    labelsAsStrings.includes(label)
+  );
+
+  if (workflowIssue && priorityExists) {
+    return github.projects.createCard({
+      column_id: 9962014,
+      content_id: context.payload.issue.id,
+      content_type: "Issue",
+    });
+  } else {
+    return console.log("Not ready for Eng", context.payload);
+  }
 };
