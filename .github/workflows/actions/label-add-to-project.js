32db89f30bfe6830840d14cf491db3c1aca26191
Jay Landro <jaylandro@gmail.com>
2020-07-21 09:02:16 -0500

    Try/catch

--- .github/workflows/actions/label-add-to-project.js
+++ .github/workflows/actions/label-add-to-project.js
@@ -13,22 +13,29 @@ module.exports = ({ context, github }) => {
   }
 
   async function labelAndAdd(project) {
-    await github.issues.addLabels({
-      issue_number: currentIssue.number,
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      labels: project.labels,
-    });
+    try {
+      await github.issues.addLabels({
+        issue_number: currentIssue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        labels: project.labels,
+      });
 
-    await github.projects.createCard({
-      column_id: project.column_id,
-      content_id: currentIssue.id,
-      content_type: "Issue",
-    });
+      await github.projects.createCard({
+        column_id: project.column_id,
+        content_id: currentIssue.id,
+        content_type: "Issue",
+      });
 
-    console.log(
-      `Added Labels: ${project.labels} & added to Project Column ${project.column_id}`
-    );
+      console.log(
+        `Added Labels: ${project.labels} & added to Project Column ${project.column_id}`
+      );
+    } catch (error) {
+      if (error[0].message === "Project already has the associated issue") {
+        return;
+      }
+      console.error("Label and add failed with:", error);
+    }
   }
-  console.log("Context", context);
+  console.log("Context: ", context);
 };
