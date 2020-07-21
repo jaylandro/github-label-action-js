a45a113a66c205da8b076462d0850ac5d6a3ea98
Jay Landro <jaylandro@gmail.com>
2020-07-21 08:30:45 -0500

    Async test

--- .github/workflows/actions/label-add-to-project.js
+++ .github/workflows/actions/label-add-to-project.js
@@ -1,31 +1,31 @@
 module.exports = ({ context, github }) => {
-  const issue = context.payload.issue;
-  let selectedProject;
+  const labelName = context.payload.label.name;
 
   const project = {
     column_id: 9962014,
     components: ["c: SQL Editor", "Bug", "WIP"],
-    label: "automated!"
+    label: "automated!",
+  };
+
+  if (project.components.includes(labelName)) {
+    labelAndAdd(project);
   }
 
-  if (project.components.includes(issue.label)) {
-    selectedProject = project;
-  }
-
-  if (selectedProject) {
+  async function labelAndAdd() {
     await github.issues.addLabels({
       issue_number: context.payload.issue.number,
       owner: context.repo.owner,
       repo: context.repo.repo,
       labels: selectedProject.label,
-    })
+    });
     await github.projects.createCard({
       column_id: selectedProject.column_id,
       content_id: context.payload.issue.id,
       content_type: "Issue",
-    })
-    console.log('Label Added:', labelAdded, 'Added to Project', addedToProject)
-  } else {
-    console.log("Not ready for Eng", context.payload);
+    });
   }
+  console.log("Label Added:", labelAdded, "Added to Project", addedToProject);
+  // } else {
+  //   console.log("Not ready for Eng", context.payload);
+  // }
 };
